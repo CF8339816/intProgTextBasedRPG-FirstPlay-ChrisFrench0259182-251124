cb93892a022c1afa00fa33a794cece9f39b1a9ef
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-17 09:18:34 -0400

    Initial commit

---
+++ intProgTextBasedRPG-FirstPlay-ChrisFrench0259182-251124/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+namespace intProgTextBasedRPG_FirstPlay_ChrisFrench0259182_251124
+{
+    internal class Program
+    {
+
+
+
+
+        static string filepath = "maps.txt";
+
+
+
+       
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ intProgTextBasedRPG-FirstPlay-ChrisFrench0259182-251124/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("intProgTextBasedRPG-FirstPlay-ChrisFrench0259182-251124")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("intProgTextBasedRPG-FirstPlay-ChrisFrench0259182-251124")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e52fe881-9f77-4eca-9664-3fa8e946d106")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

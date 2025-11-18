f9bb9a7b34e5bd233d4eba63a36b80fd54ea33c0
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-18 11:33:41 -0400

    adding player  and player move function

--- intProgTextBasedRPG-FirstPlay-ChrisFrench0259182-251124/Program.cs
+++ intProgTextBasedRPG-FirstPlay-ChrisFrench0259182-251124/Program.cs
@@ -14,26 +14,42 @@ namespace intProgTextBasedRPG_FirstPlay_ChrisFrench0259182_251124
 
         static Random healthPackSpawn = new Random();
         static Random EnemyStartSpawn = new Random();
+        static int p1_x_input;
+        static int p1_y_input;
+        static int enemy_x_input;
+        static int enemy_y_input;
 
-
-
-
+        static (int, int) p1_min_max_x = (1, 29);
+        static (int, int) p1_min_max_y = (1, 32);
+        static (int, int) p2_min_max_x = (1, 29);
+        static (int, int) p2_min_max_y = (1, 32);
 
 
 
         static string filepath = "maps.txt";
+        static int p1_KillScore = 0;
 
-      
+
+        static bool healthTreasure = true;
+        static bool EnemySpawn = true;
+
+
+        static int turn = -1;
+
+        static char[] allKeybindings = (new char[] { 'W', 'A', 'S', 'D' });
+        static ConsoleColor[] playerColors = { ConsoleColor.Cyan};
+        static ConsoleColor[] enemyColors = { ConsoleColor.Red };
+        static ConsoleColor[] treasureColors = { ConsoleColor.Magenta };
 
         static void Main(string[] args)
         {
 
-            while (isPlaying)
+            
 
             DrawMap();
             mapLegend();
 
-
+            while (isPlaying)
 
 
 
@@ -53,47 +69,41 @@ namespace intProgTextBasedRPG_FirstPlay_ChrisFrench0259182_251124
                 foreach (string map in Maps)
                 {
 
-                    //if (map = "g")
-                    //{
-                    //    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    //}
-                    //if (map = "w")
-                    //{
-                    //    Console.ForegroundColor = ConsoleColor.Blue;
-                    //}
-                    //if (map = "m")
-                    //{
-                    //    Console.ForegroundColor = ConsoleColor.Gray;
-                    //}
-                    //if (map = "t")
-                    //{
-                    //    Console.ForegroundColor = ConsoleColor.Green;
-                    //}
-
-
-
-                    switch (map)
+                    foreach (char mapChar in map)
                     {
 
-                        case "g": // Grass
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            break;
-                        case "w": // Water
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            break;
-                        case "m": // Mountain
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            break;
-                        case "t": // Trees
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            break;
+                       switch (mapChar)
+                        
+                        {
+                           
+                            case 'g': // Grass
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                break;
+                            
+                            case 'w': // Water
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                break;
+                            
+                            case 'm': // Mountain
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                break;
+                           
+                            case 't': // Trees
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                break;
 
 
+                        }
+                        
+                        Console.Write(mapChar);
                     }
-
-                    Console.WriteLine(map); 
+                    
+                    Console.WriteLine();
                 }
 
+               
+                Console.ResetColor();
+            
 
                 
             }
@@ -112,11 +122,7 @@ namespace intProgTextBasedRPG_FirstPlay_ChrisFrench0259182_251124
 
             }
         //m2
-
-
-       
-        //m3
-
+        
         static void mapLegend()
         {
 
@@ -142,6 +148,47 @@ namespace intProgTextBasedRPG_FirstPlay_ChrisFrench0259182_251124
 
             Console.ResetColor();
         }
+
+        //m3
+
+        static void ProcessInput()
+        {
+            // if this isn't here, input will block the game before drawing for the first time
+            if (turn == -1) return;
+
+            // reset input
+            p1_x_input = 0;
+            p1_y_input = 0;
+            enemy_x_input = 0;
+           enemy_y_input = 0;
+
+            char[] allowedKeysThisTurn; // different keys allowed on p1 vs. p2 turn
+
+            // choose which keybindings to use
+            if (turn % 2 == 0)
+            {
+                allowedKeysThisTurn = allKeybindings;
+            }
+            else
+            {
+                allowedKeysThisTurn = (allKeybindings);
+            }
+                // get the current player's input
+                ConsoleKey input = ConsoleKey.NoName;
+            while (allowedKeysThisTurn.Contains(((char)input)))
+            {
+                input = Console.ReadKey(true).Key;
+            }
+
+            // check all input keys
+            if (input == ConsoleKey.A) p1_x_input = -1;
+            if (input == ConsoleKey.D) p1_x_input = 1;
+            if (input == ConsoleKey.W) p1_y_input = -1;
+            if (input == ConsoleKey.S) p1_y_input = 1;
+
+           
+        }
+
 
     }
 }
